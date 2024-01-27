e257088ae0f9a5406ae45bc3854fcb83c3348cf0
oscarbutler <>
2024-01-27 18:28:23 +0000

    Render user information from github

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,13 +1,45 @@
-function fetchGitHubInforamtion(event) {
+function userInformationHTML(user) {
+    return `
+        <h2>${user.name}
+            <span class="small-name">
+                <@<a href="${user.html_url}" target="_blank">${user.login}</a>
+            </span>
+        </h2>
+        <div class="gh-avatar">
+            <a href="${user.html_url}" target="_blank">
+                <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}"
+            </a>
+        </div>
+        <p>Followers: ${user.followers} - following ${user.following} <br> Respos: ${user.public_repos}</p>
+    </div>`;
+}
+
+function fetchGitHubInformation(event) {
 
     var username = $("#gh-username").val();
-    if (!username){
-        ("#gh-user-data").html(`<h2>Please enter a Github username</h2>`);
-    return;
+    if (!username) {
+        $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
+        return;
     }
-    $("gh-user-data").html(
+
+    $("#gh-user-data").html(
         `<div id="loader">
-            <img src="assets/css/loader.gif" alt="loading..."/>
-        </div>`
-    )
+            <img src="assets/css/loader.gif" alt="loading..." />
+        </div>`);
+
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+        ).then(
+            function(response) {
+                var userdata = response;
+                $("hg-user-data").html(userInformationHTML(userdata));
+            }, function(errorResponse) {
+                if (errorResponse.status === 404) {
+                    $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+            })
 }
